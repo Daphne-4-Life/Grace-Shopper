b73c2be4eb2aa1e715401a318e5ca551383beee4
Nam Nguyen <nguyen.x.nam@gmail.com>
2020-08-04 15:14:27 -0400

    User routes built

--- server/api/users.js
+++ server/api/users.js
@@ -15,3 +15,77 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+
+router.post('/', async (req, res, next) => {
+  try {
+    const {
+      email,
+      password,
+      googleId,
+      isAdmin,
+      address,
+      imageUrl,
+      firstName,
+      lastName
+    } = req.body
+    const user = await User.create({
+      email,
+      password,
+      googleId,
+      isAdmin,
+      address,
+      imageUrl,
+      firstName,
+      lastName
+    })
+    res.json(user)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: {id: req.params.userId},
+      attributes: [
+        'id',
+        'email',
+        'address',
+        'imageUrl',
+        'firstName',
+        'lastName'
+      ]
+    })
+    res.json(user)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(+req.params.id)
+    if (user) {
+      await user.destroy()
+      res.sendStatus(204)
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:userId', async (req, res, next) => {
+  try {
+    const {updatedFields} = req.body
+    const user = await User.update(
+      {...updatedFields},
+      {where: {id: req.params.userId}}
+    )
+    res.json(user)
+  } catch (error) {
+    next(error)
+  }
+})

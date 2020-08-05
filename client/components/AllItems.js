7c5472e51e12618f1442bf6d54540e8f2c97331c
Blake Ferm <blakeferm@gmail.com>
2020-08-05 13:58:38 -0400

    removed delete functionality from all items component

--- client/components/AllItems.js
+++ client/components/AllItems.js
@@ -52,8 +52,7 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-  getItems: () => dispatch(fetchItems()),
-  deleteItem: id => dispatch(deleteItem(id))
+  getItems: () => dispatch(fetchItems())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(AllItems)

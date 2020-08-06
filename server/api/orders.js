248d7f694875e1a32377c5f0445defb7e38faa66
jennagiordano <31827859+jennagiordano@users.noreply.github.com>
2020-08-06 11:37:59 -0400

    Updates to Orders API / reducer

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const {Order} = require('../db/models')
+const {OrderContent} = require('../db/models')
 module.exports = router
-
 router.get('/', async (req, res, next) => {
   try {
     const orders = await Order.findAll(req.body)
@@ -10,16 +10,31 @@ router.get('/', async (req, res, next) => {
     next(error)
   }
 })
-
-router.get('/:id', async (req, res, next) => {
+router.get('/:userId', async (req, res, next) => {
   try {
-    const orderId = await Order.findByPk(req.params.id)
-    res.json(orderId)
+    const userOrders = await Order.findAll({
+      where: {
+        userId: req.params.userId
+      }
+    })
+    res.json(userOrders)
   } catch (error) {
     next(error)
   }
 })
-
+router.get('/:userId/cart', async (req, res, next) => {
+  try {
+    const userOrders = await Order.findAll({
+      where: {
+        userId: req.params.userId,
+        status: 'pending'
+      }
+    })
+    res.json(userOrders)
+  } catch (error) {
+    next(error)
+  }
+})
 router.post('/', async (req, res, next) => {
   try {
     const addOrder = await Order.create(req.body)
@@ -28,25 +43,19 @@ router.post('/', async (req, res, next) => {
     next(error)
   }
 })
-
-router.put('/:id', async (req, res, next) => {
+router.put('/:userId/cart', async (req, res, next) => {
   try {
-    const order = await Order.findByPk(req.params.id)
-    const updatedOrder = await order.update(req.body)
-    res.json(updatedOrder)
-  } catch (error) {
-    next(error)
-  }
-})
-
-router.delete('/:id', async (req, res, next) => {
-  try {
-    const deleteOrder = await Order.destroy({
+    const order = await Order.findOne({
+      include: {
+        model: OrderContent
+      },
       where: {
-        id: req.params.id
+        userId: req.params.userId,
+        status: 'pending'
       }
     })
-    res.json(deleteOrder)
+    const updatedOrder = await order.update(req.body)
+    res.json(updatedOrder)
   } catch (error) {
     next(error)
   }

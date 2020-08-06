80b4e8d7bc70ec454102dc5c07b9768bd2c7bf6a
Nam Nguyen <nguyen.x.nam@gmail.com>
2020-08-06 18:03:38 -0400

    Profile updated, need to fix previous orders, added new route to add new items to cart, working on seed file

--- server/db/models/orderContent.js
+++ server/db/models/orderContent.js
@@ -6,19 +6,6 @@ const OrderContent = db.define('OrderContent', {
     type: Sequelize.INTEGER,
     allowNull: false,
     defaultValue: 0
-  },
-  price: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    defaultValue: 0
-  },
-  color: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  size: {
-    type: Sequelize.STRING,
-    allowNull: false
   }
 })
 module.exports = OrderContent

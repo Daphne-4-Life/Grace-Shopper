3b8d1afc568df834cbe2820014ff0c3ed27807fe
jennagiordano <31827859+jennagiordano@users.noreply.github.com>
2020-08-07 17:16:01 -0400

    WIP - Updates to Add to Cart functionality

--- server/db/models/orderContent.js
+++ server/db/models/orderContent.js
@@ -6,6 +6,12 @@ const OrderContent = db.define('OrderContent', {
     type: Sequelize.INTEGER,
     allowNull: false,
     defaultValue: 0
+  },
+  color: {
+    type: Sequelize.STRING
+  },
+  size: {
+    type: Sequelize.STRING
   }
 })
 module.exports = OrderContent
